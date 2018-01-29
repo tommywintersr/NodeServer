00ff75ca274e2e5d2c0b2d3270637e2a9d9d5951
Ubuntu <ubuntu@ip-172-31-29-81.us-east-2.compute.internal>
2018-01-29 05:40:07 +0000

    Add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'NodeServer',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-216-91-250.us-east-2.compute.amazonaws.com',
+      key: '~/keys/tommy_key.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:tommywintersr/NodeServer',
+      path: '/home/ubuntu/server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
