e9a4aec738889f41e93b94593206207d4282a5a4
Thomas Winter <thomas89winter@gmail.com>
2018-01-29 20:26:24 -0800

    update AWS host

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-216-91-250.us-east-2.compute.amazonaws.com',
+      host: 'ec2-13-58-82-229.us-east-2.compute.amazonaws.com',
       key: '~/keys/tommy_key.pem',
       ref: 'origin/master',
       repo: 'git@github.com:tommywintersr/NodeServer',
